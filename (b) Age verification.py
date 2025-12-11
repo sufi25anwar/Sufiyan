ec921c028101cfc7ab97bf67efd2093e1760f648
sufi25anwar <sufi.007.anwar@gmail.com>
2025-12-11 20:22:00 +0530

    Add files via upload

---
+++ (b) Age verification.py
@@ -0,0 +1,9 @@
+age = int (input ("enter age:"))
+if  age< 13:
+       print("child")
+elif  age < 19 > 13:
+      print("teenager")
+elif  age < 64 > 20:
+      print("adult")
+else:  
+      print("senior")    
---
+++ (c) Two number.py
@@ -0,0 +1,7 @@
+a = int (input ("enter no1:"))
+b = int (input ("enter no2:"))
+if a > b:
+     print(a)
+elif a == b:
+     print("a=b")
+     
